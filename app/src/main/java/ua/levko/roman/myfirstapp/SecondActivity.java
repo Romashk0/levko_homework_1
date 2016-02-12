16efda908177d51814b4e3001d95edc3994122d4
Roman Levko <pifko22@gmail.com>
2016-02-12 16:04:53 +0200

    changes

--- app/src/main/java/ua/levko/roman/myfirstapp/SecondActivity.java
+++ app/src/main/java/ua/levko/roman/myfirstapp/SecondActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
  * Created by Comp REM on 12.02.2016.
  */
 public class SecondActivity extends Activity {
+   @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
