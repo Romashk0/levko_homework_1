11a8835a74216d0a22e7ac0f6d77c515db2870c6
Roman Levko <pifko22@gmail.com>
2016-02-12 13:32:42 +0200

    first

---
+++ app/src/main/java/ua/levko/roman/myfirstapp/FirstActivity.java
@@ -0,0 +1,18 @@
+package ua.levko.roman.myfirstapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.provider.CalendarContract;
+
+/**
+ * Created by Comp REM on 12.02.2016.
+ */
+public class FirstActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.firstmain);
+
+    }
+}
---
+++ app/src/main/java/ua/levko/roman/myfirstapp/SecondActivity.java
@@ -0,0 +1,15 @@
+package ua.levko.roman.myfirstapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Comp REM on 12.02.2016.
+ */
+public class SecondActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secondlayout);
+
+    }
+}
